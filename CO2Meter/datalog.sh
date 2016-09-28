089c5df7689f023dace92b20f4a1e8105975cec3
r4space <r4space@gmail.com>
2016-09-28 03:43:56 +0000

    onboard commit

--- CO2Meter/datalog.sh
+++ CO2Meter/datalog.sh
@@ -7,7 +7,7 @@ echo "Hello log"
 sudo modprobe -r i2c_bcm2708
 sudo modprobe i2c_bcm2708 baudrate=9600
 
-sleep 1
+sleep 30
 echo "ok made through 30 second delay"
 
 python /home/pi/sphex/CO2Meter/readData.py
