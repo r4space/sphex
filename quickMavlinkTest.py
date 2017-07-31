7d186cb3485dea0aa2680eae116383ddfd0587bf
r4space <r4space@gmail.com>
2017-07-31 01:09:53 -0400

    Inserted correct Pi-PH connection instructions into quickMavlinkTest.py for reference - new instructions pertain to Pi3s only

--- quickMavlinkTest.py
+++ quickMavlinkTest.py
@@ -1,6 +1,8 @@
 from dronekit import connect
 #vehicle = connect('/dev/ttyAMA0', wait_ready=True, baud=9600)
-vehicle = connect('/dev/ttyAMA0', wait_ready=True, baud=57600)
+#vehicle = connect('/dev/ttyAMA0', wait_ready=True, baud=57600)
+vehicle = connect('/dev/serial0', wait_ready=True, baud=57600)
+
 print " GPS: %s" % vehicle.gps_0
 
 vehicle.close()
